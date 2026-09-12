c5b4a2568c71d2de5d84a104f675af36ad54fb1e
Pedro Ferreira <pedrohenriquedez12@gmail.com>
2026-09-12 12:08:12 -0300

    Compelemento da previsao especifica
    Adicionado o restante do código da previsão específica

--- AULA2/submodulo/PrevisaoEspecifica.py
+++ AULA2/submodulo/PrevisaoEspecifica.py
@@ -3,7 +3,17 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 dados = pd.read_csv('vendas.csv')
+st.write(dados)
 
-df = pd.DataFrame(dados)
+#Organizar os dados
+x = dados[['mes']]
+y = dados['vendas']
 
-print(df)
+# Treinar o modelo
+model = LinearRegression()
+model.fit(x, y)
+mes = 9
+
+df = model.predict(pd.DataFrame({'mes': [mes]}))[0]
+
+st.write(f'Previsao do mes: R${df:.2f}')
